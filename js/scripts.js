be9835125009b29bbfc0c4e24a19a19891c5dcb4
Jasmine Gadie <jasmine.booyakka@gmail.com>
2018-02-12 20:38:15 -0800

    show and hide story in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function() {
+  $("#blanks form").submit(function(event) {
     $(".person1").append("blah blah");
     $(".person2").append("blah blah");
     $(".animal").append("blah blah");
@@ -8,5 +8,7 @@ $(document).ready(function() {
     $(".noun").append("blah blah");
 
     $("#story").show();
+
+    event.preventDefault();
   });
 });
